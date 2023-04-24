5997e84b1a9de27e1f46adc39e296f964d455667
Stuart Hagler <sehagler@gmail.com>
2023-04-24 14:40:22 -0700

    FUNCTION - Add sections_main files to all projects 	   Add tools files for new biomarkers project

---
+++ development/query_lib/processor_lib/breast_cancer_biomarkers_ar_tools.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 15:52:26 2022
+
+@author: haglers
+"""
+
+#
+import re
+
+#
+from query_lib.processor_lib.base_lib.breast_cancer_biomarkers_tools_base \
+    import Preprocessor as Preprocessor_base
+from query_lib.processor_lib.base_lib.breast_cancer_biomarkers_tools_base \
+    import Postprocessor as Postprocessor_base
+  
+#
+class Postprocessor(Postprocessor_base):
+    pass
+  
+#
+class Preprocessor(Preprocessor_base):
+    pass
---
+++ development/query_lib/processor_lib/breast_cancer_biomarkers_pdl1_tools.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  4 15:52:26 2022
+
+@author: haglers
+"""
+
+#
+from query_lib.processor_lib.base_lib.breast_cancer_biomarkers_tools_base \
+    import Preprocessor as Preprocessor_base
+from query_lib.processor_lib.base_lib.breast_cancer_biomarkers_tools_base \
+    import Postprocessor as Postprocessor_base
+    
+#
+class Postprocessor(Postprocessor_base):
+    pass
+  
+#
+class Preprocessor(Preprocessor_base):
+    pass
