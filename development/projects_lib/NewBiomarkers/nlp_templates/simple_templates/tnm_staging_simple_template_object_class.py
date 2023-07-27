fa93da669644ff02db407a8d3a397d7e7ee34421
Stuart Hagler <sehagler@gmail.com>
2023-07-27 15:10:56 -0700

    FUNCTION - remove directory_manager tested for 	   NewBiomarkers

--- development/projects_lib/NewBiomarkers/nlp_templates/simple_templates/tnm_staging_simple_template_object_class.py
+++ development/projects_lib/NewBiomarkers/nlp_templates/simple_templates/tnm_staging_simple_template_object_class.py
@@ -6,16 +6,11 @@ Created on Wed Mar 16 17:18:08 2022
 """
 
 #
-from base_lib.manager_base_class import Manager_base
 from query_lib.processor_lib.tnm_staging_tools \
     import simple_template as tnm_staging_simple_template
 
 #
-class Tnm_staging_simple_template_manager(Manager_base):
-    
-    #
-    def __init__(self, static_data_object, logger_object):
-        Manager_base.__init__(self, static_data_object, logger_object)
+class Tnm_staging_simple_template_object(object):
     
     #
     def simple_template(self):
